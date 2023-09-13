e9c979b9b2e99888fe705a0be98af310166f3a5c
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-09-12 21:21:25 -0700

    Memoized

--- dynnamic practice/CanSum.java
+++ dynnamic practice/CanSum.java
@@ -1,25 +1,35 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class CanSum {
 
     public static void main(String args[]) {
-        System.out.println(can(6, new int[] { 1, 2, 3 }));
+        System.out.println(can(6, new int[] { 4, 5 }));
     }
 
     private static boolean can(int target, int[] nums) {
-        HashSet<Integer> seen = new HashSet<>();
-        int diff, cur;
-        for (int i = 0; i < nums.length; i++) {
-            cur = nums[i];
-            diff = target - cur;
-            if (diff == 0) {
-                return true;
-            }
-            if (seen.contains(diff)) {
-                return true;
-            }
-            seen.add(cur);
+        HashMap<Integer, Boolean> memo = new HashMap<>();
+        return can(target, nums, memo);
+    }
+
+    private static boolean can(int target, int[] nums, HashMap<Integer, Boolean> memo) {
+        if (memo.containsKey(target)) {
+            return memo.get(target);
         }
+        if (target == 0) {
+            return true;
+        }
+        if (target < 0) {
+            return false;
+        }
+        int rem;
+        for (int num : nums) {
+            rem = target - num;
+            if (can(rem, nums, memo)) {
+                memo.put(target, true);
+                return true;
+            }
+        }
+        memo.put(target, false);
         return false;
     }
 }
