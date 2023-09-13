acc0698241ad27c67e30a7027e31ad687a306f2f
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-09-12 17:53:40 -0700

    Dynamic Practice 2

---
+++ dynnamic practice/GridTravel.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class GridTravel {
+    // In how many ways can you traverse a grid with n * m dimensions if you start
+    // in the top left and end in the bottom right?
+    // Assume you can only move right and down
+    public static void main(String args[]) {
+        System.out.println(calcPaths(15, 15));
+    }
+
+    private static int calcPaths(int n, int m) {
+        HashMap<String, Integer> memo = new HashMap<>();
+        return calcPaths(n, m, memo);
+    }
+
+    private static int calcPaths(int n, int m, HashMap<String, Integer> memo) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        String key = n + "," + m;
+        if (memo.containsKey(key))
+            return memo.get(key);
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        memo.put(key, calcPaths(n - 1, m, memo) + calcPaths(n, m - 1, memo));
+        return memo.get(key);
+    }
+}
